51d972b52513ffc43be50dd79e0f3530efb665a9
tdeyusij <gs1jhondase@hotmail.com>
2024-05-06 10:59:45 +0700

    test commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const nthElement = (arr, n = 0) =>
-  (n > 0 ? arr.slice(n, n + 1) : arr.slice(n))[0];
+function combinationSum3(k, n) {
+  const result = [];
+  backtrack([], 1, k, n);
+  return result;
+  function backtrack(combination, start, k, n) {
+    if (n === 0 && k === 0) {
+      result.push([...combination]);
+      return;
+    }
+    if (n < 0 || k === 0) return;
+    for (let i = start; i <= 9; i++) {
+      combination.push(i);
+      backtrack(combination, i + 1, k - 1, n - i);
+      combination.pop();
+    }
+  }
+}
