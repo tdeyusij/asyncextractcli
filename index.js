667be30ab188b78194697033caac62a78a0a9c41
tdeyusij <gs1jhondase@hotmail.com>
2024-05-07 17:52:23 +0700

    update project

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-function combinationSum3(k, n) {
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
   const result = [];
-  backtrack([], 1, k, n);
-  return result;
-  function backtrack(combination, start, k, n) {
-    if (n === 0 && k === 0) {
-      result.push([...combination]);
-      return;
-    }
-    if (n < 0 || k === 0) return;
-    for (let i = start; i <= 9; i++) {
-      combination.push(i);
-      backtrack(combination, i + 1, k - 1, n - i);
-      combination.pop();
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      let low = i + 1;
+      let high = nums.length - 1;
+      let sum = 0 - nums[i];
+      while (low < high) {
+        if (nums[low] + nums[high] === sum) {
+          result.push([nums[i], nums[low], nums[high]]);
+          while (low < high && nums[low] === nums[low + 1]) low++;
+          while (low < high && nums[high] === nums[high - 1]) high--;
+          low++;
+          high--;
+        } else if (nums[low] + nums[high] < sum) low++;
+        else high--;
+      }
     }
   }
+  return result;
 }
