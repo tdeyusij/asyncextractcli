d04e013f1aeb2c64455b745ad6eec8b41caf9d99
tdeyusij <gs1jhondase@hotmail.com>
2024-05-09 18:58:57 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function mergeKLists(lists) {
-  if (lists.length === 0) return null;
-  while (lists.length > 1) {
-    const first = lists.shift();
-    const second = lists.shift();
-    const merged = mergeTwoLists(first, second);
-    lists.push(merged);
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(permutation) {
+    if (permutation.length === nums.length) {
+      result.push([...permutation]);
+      return;
+    }
+    for (const num of nums) {
+      if (!permutation.includes(num)) {
+        permutation.push(num);
+        backtrack(permutation);
+        permutation.pop();
+      }
+    }
   }
-  return lists[0];
 }
