c1dd0bdd728d4284c2a2bbd09416d6d80ef14f8d
tdeyusij <gs1jhondase@hotmail.com>
2024-05-11 03:51:53 +0700

    demo task

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-function permute(nums) {
-  const result = [];
-  backtrack([]);
-  return result;
-  function backtrack(permutation) {
-    if (permutation.length === nums.length) {
-      result.push([...permutation]);
-      return;
-    }
-    for (const num of nums) {
-      if (!permutation.includes(num)) {
-        permutation.push(num);
-        backtrack(permutation);
-        permutation.pop();
-      }
-    }
-  }
-}
+console.log("world");
